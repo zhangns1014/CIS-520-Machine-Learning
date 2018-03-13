34c77fd852d718855e71d6e804e594b938917e33
zhangns <33401873+zhangns1014@users.noreply.github.com>
2018-03-13 18:54:25 -0400

    Delete LogisticRegressionL2.m

--- LogisticRegressionL2.m
+++
@@ -1,9 +0,0 @@
-function [w, b] = LogisticRegressionL2(traindata, trainlabels, lambda)
-    % leave the bias unregularized
-   x=[traindata,ones(size(traindata,1),1)];
-   y=trainlabels;
-   L=@(w_aug)(sum(log(1+exp(-y.*(x*w_aug)))-lambda*w_aug(1:end-1)'*w_aug(1:end-1)));
-   w_aug_solution=fminunc(L,[zeros(57,1);0]);
-   w=w_aug_solution(1:end-1,1);
-   b=w_aug_solution(end,1);
-end
